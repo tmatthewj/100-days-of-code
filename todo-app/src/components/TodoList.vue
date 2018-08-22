4046c9079465dd2822d6a7b4f38e1139537f7ea5
Matthew John <matthew@typito.com>
2018-08-22 12:37:26 +0530

    todo-app template

---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul>
+            <li> Todo A </li>
+            <li> Todo B </li>
+            <li> Todo C </li>
+        </ul>
+    </div>
+</template>
+
+<script type = "text/javascript" >
+
+export default {
+};
+
+</script>
+
+
+<style>
+
+</style>
